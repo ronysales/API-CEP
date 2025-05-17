d47f63fe93d9656e13dd385ecadfdaf33998b2d4
ronysales <ronysales@gmail.com>
2025-05-17 09:24:25 -0300

    O Site agora vai funcionar tentativa 17! se Deus Quiser

--- app.py
+++ app.py
@@ -20,6 +20,12 @@ def pesquisacep(cep):
     resposta = requests.get(url)
     return resposta.json()
 
+@app.route('/tempo/',methods=['GET'])
+def tempo():
+    url = f'https://api.weatherapi.com/v1/current.json?key=c4380707dde242f4b78202712252204&q=Sao Paulo&lang=pt'
+    resposta = requests.get(url)
+    return resposta.json()
+
 
 if __name__== '__main__':
     app.run(debug=True)
